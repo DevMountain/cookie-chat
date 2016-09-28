2beeb5642eb0c3357cc9df2c22d1b142a4332bc5
Brett Caudill <brett.caudillo@gmail.com>
2016-09-28 13:05:40 -0600

    Revert "Added Filter"

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,21 +1,20 @@
 angular.module('chatroom').controller('mainCtrl', function($scope, messageService){
-  //In your controller you'll have a getMessages function and a postMessage function,
-  //but should be placed on $scope.
+  //In your controller you'll have a getMessages function and a postMessage function, but should be placed on $scope.
 
-  //The getMessages function will call the getData method on the messageService object.
-  //You'll then save the result of that request to
+  //The getMessages function will call the getData method on the messageService object. You'll then save the result of that request to
   //your controllers $scope as messages ($scope.messages)
 
 
 
-  //The postMessage function will take whatever the user typed in (hint: look at
-  //the html and see what ng-model correlates to on the input box),
-  //pass that text to the postMessage method on the messageService object which will
-  //then post it to the backend.
+  //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
+  //pass that text to the postMessage method on the messageService object which will then post it to the backend.
+
 
 
 
   //uncomment this code when your getMessages function is finished
   //This goes and gets new data every second, which mimicking a chat room experience.
-
+  // setInterval(function(){
+  //   $scope.getMessages();
+  // }, 1500)
 })
