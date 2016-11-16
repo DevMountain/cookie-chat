92195fd9f9fce802ea79d989e9e651d6105363e4
rnoelle <reidnoelle2@gmail.com>
2016-11-16 16:46:27 -0700

    fixed README and instructions to make function names consistent

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,12 +1,12 @@
 angular.module('chatroom').controller('mainCtrl', function($scope, messageService){
 
-  //The getMessages function will call the getData method on the messageService object.
+  //The getMessages function will call the getMessages method on the messageService object.
   //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
 
 
 
 
-  //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box), 
+  //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
   //pass that text to the postMessage method on the messageService object which will
   //then post it to the backend.
 
