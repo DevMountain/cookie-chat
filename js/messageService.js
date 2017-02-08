946e85ab8a3e38ce581826de47c74ae96e234516
Kedirech <brackcarmony@gmail.com>
2017-02-08 12:51:56 -0700

    Removed Parse script, and removed a reference to it.

--- js/messageService.js
+++ js/messageService.js
@@ -10,8 +10,8 @@ angular.module('chatroom').service('messageService', function($http){
 
   //On the line below create the postMessage method. This method will add data to the backend server.
   //The url for the request needs to be 'https://practiceapi.devmounta.in/api/chats'
-  //Because we're making a POST request, we need a way to tell the server the data we want to give it, in your $http call (along with url and method) have a data property which has a value that is equal to another object with a key of message and a value of the message being passed to parse. IE data: {message: yourMessage}
-  //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), you can then use .then in your controller.
+  //Because we're making a POST request, need a data object with a message property IE data: {message: "Hello World"}
+  //Also, remember that $http returns a promise. So return the whole $http call (return $http(...)), so you can then use .then in your controller.
 
 
 
